161b9b29fd753f12da981f3c406cde08571ee0b0
Matt Thomas <matt@betweenbrain.com>
2013-07-15 15:14:32 -0400

    Kinda helps to have the ul element

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,7 +8,10 @@
  * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v3 or later
  */
-
-foreach ($items as $item) {
-	echo '<li class="item' . $item->id . ' ' . $item->alias . '"><a href="' . JRoute::_($item->link . '&Itemid=' . $item->id) . '"/>' . $item->name . '</a></li>';
-}
+?>
+<ul>
+	<?php foreach ($items as $item) {
+		echo '<li class="item' . $item->id . ' ' . $item->alias . '"><a href="' . JRoute::_($item->link . '&Itemid=' . $item->id) . '"/>' . $item->name . '</a></li>';
+	}
+	?>
+</ul>
