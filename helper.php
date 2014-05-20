efa4a987dc7819d110b9ae22ddf4c17ad7f3975c
Matt Thomas <matt@betweenbrain.com>
2014-05-20 10:00:25 -0400

    Code formatting, again

--- helper.php
+++ helper.php
@@ -11,16 +11,19 @@
 
 jimport('joomla.application.menu');
 
-class modMenuwrenchHelper {
+class modMenuwrenchHelper
+{
 
 	/**
 	 * Constructor
 	 *
-	 * @param JRegistry $params: module parameters
+	 * @param JRegistry $params : module parameters
+	 *
 	 * @since 0.1
 	 *
 	 */
-	public function __construct($params) {
+	public function __construct($params)
+	{
 		$this->app    = JFactory::getApplication();
 		$this->menu   = $this->app->getMenu();
 		$this->active = $this->menu->getActive();
@@ -34,19 +37,22 @@ class modMenuwrenchHelper {
 	 * @since 0.1
 	 *
 	 */
-	function getBranches() {
-		$renderedItems     = $this->params->get('renderedItems', 0);
-		$showSubmenu = $this->params->get('showSubmenu');
-		$hideSubmenu  = $this->params->get('hideSubmenu', 0);
+	function getBranches()
+	{
+		$renderedItems = $this->params->get('renderedItems', 0);
+		$showSubmenu   = $this->params->get('showSubmenu');
+		$hideSubmenu   = $this->params->get('hideSubmenu', 0);
 		// http://stackoverflow.com/questions/3787669/how-to-get-specific-menu-items-from-joomla/10218419#10218419
-		$items = $this->menu->getItems(NULL, NULL);
+		$items = $this->menu->getItems(null, null);
 
 		// Convert renderedItems to an array if only one item is selected
-		if (!is_array($renderedItems)) {
+		if (!is_array($renderedItems))
+		{
 			$renderedItems = str_split($renderedItems, strlen($renderedItems));
 		}
 
-		if (!is_array($hideSubmenu)) {
+		if (!is_array($hideSubmenu))
+		{
 			$hideSubmenu = str_split($hideSubmenu, strlen($hideSubmenu));
 		}
 
@@ -54,24 +60,28 @@ class modMenuwrenchHelper {
 		 * Builds menu hierarchy by nesting children in parent object's 'children' property.
 		 * First builds an item Id based array, then discards old nodes.
 		 */
-		foreach ($items as $key => $item) {
+		foreach ($items as $key => $item)
+		{
 
 			$items[$item->id] = $item;
 
 			unset($items[$key]);
 
-			if ($item->parent_id != 1) {
+			if ($item->parent_id != 1)
+			{
 				$items[$item->parent_id]->children[$item->id] = $item;
 			}
 		}
 
-		foreach ($items as $key => $item) {
+		foreach ($items as $key => $item)
+		{
 
 			/**
 			 * Remove non-selected menu item objects
 			 * At this point, all selected items to render are in the first level of the array
 			 */
-			if (!in_array($key, $renderedItems)) {
+			if (!in_array($key, $renderedItems))
+			{
 				unset($items[$key]);
 			}
 
@@ -81,22 +91,26 @@ class modMenuwrenchHelper {
 			$item->class = 'item' . $item->id . ' ' . $item->alias;
 
 			// Add parent class to all parents
-			if (isset($item->children)) {
+			if (isset($item->children))
+			{
 				$item->class .= ' parent';
 			}
 
 			// Add current class to specific item
-			if ($item->id == $this->active->id) {
+			if ($item->id == $this->active->id)
+			{
 				$item->class .= ' current';
 			}
 
 			// Add active class to all items in active branch
-			if (in_array($item->id, $this->active->tree)) {
+			if (in_array($item->id, $this->active->tree))
+			{
 				$item->class .= ' active';
 			}
 
 			// Hide sub-menu items if parameter set to no and parent not active
-			if ((!in_array($item->id, $this->active->tree) && $showSubmenu == 0) || in_array($item->id, $hideSubmenu)) {
+			if ((!in_array($item->id, $this->active->tree) && $showSubmenu == 0) || in_array($item->id, $hideSubmenu))
+			{
 				unset($item->children);
 			}
 		}
@@ -107,17 +121,19 @@ class modMenuwrenchHelper {
 	/**
 	 * Renders the menu
 	 *
-	 * @param $item                 : the menu item
-	 * @param string $containerTag  : optional, declare a different container HTML element
-	 * @param string $containerClass: optional, declare a different container class
-	 * @param string $itemTag       : optional, declare a different menu item HTML element
-	 * @param int $level            : counter for level of depth that is rendering.
+	 * @param        $item           : the menu item
+	 * @param string $containerTag   : optional, declare a different container HTML element
+	 * @param string $containerClass : optional, declare a different container class
+	 * @param string $itemTag        : optional, declare a different menu item HTML element
+	 * @param int    $level          : counter for level of depth that is rendering.
+	 *
 	 * @return string
 	 *
 	 * @since 0.1
 	 */
 
-	public function render($item, $containerTag = '<ul>', $containerClass = 'menu', $itemTag = '<li>', $level = 0) {
+	public function render($item, $containerTag = '<ul>', $containerClass = 'menu', $itemTag = '<li>', $level = 0)
+	{
 
 		$itemOpenTag       = str_replace('>', ' class="' . $item->class . '">', $itemTag);
 		$itemCloseTag      = str_replace('<', '</', $itemTag);
@@ -138,19 +154,24 @@ class modMenuwrenchHelper {
 				break;
 		endswitch;
 
-		if ($item->type == 'separator') {
+		if ($item->type == 'separator')
+		{
 			$output = $itemOpenTag . '<span class="separator">' . $item->name . '</span>';
-		} else {
+		}
+		else
+		{
 			$output = $itemOpenTag . '<a ' . $browserNav . ' href="' . JRoute::_($item->link . '&Itemid=' . $item->id) . '"/>' . $item->title . '</a>';
 		}
 
 		$level++;
 
-		if (isset($item->children) && $level <= $renderDepth) {
+		if (isset($item->children) && $level <= $renderDepth)
+		{
 
 			$output .= $containerOpenTag;
 
-			foreach ($item->children as $item) {
+			foreach ($item->children as $item)
+			{
 
 				$output .= $this->render($item, $containerTag, $containerClass, $itemTag, $level);
 			}
